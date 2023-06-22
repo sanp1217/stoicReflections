3006a7cb5c04d0af5b4e0e56554696ace718e0ad
sanp1217 <pablosgrad12@gmail.com>
2023-06-21 21:00:49 -0600

    Lets quotes be retained even if tab is reloaded/closed with local storage

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 import { getQuote } from "./api.js";
 
+let quotes = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const storedQuotes = localStorage.getItem("quotes");
+    if (storedQuotes) {
+        quotes = JSON.parse(storedQuotes);
+    }
+});
+
 async function processQuote() {
     try {
         const quoteData = await getQuote();
+
+        let quote = {
+            quote: quoteData.text,
+            author: quoteData.author
+        };
+
+        //quotes.push(quote);
+        localStorage.setItem("quotes", JSON.stringify(quotes));
+
         addQuote(quoteData);
     } catch (e) {
         console.log(e);
